3a976a79bfa516644228bcd5ee447cab96deafb6
Koushik K. Dutta <koushd@gmail.com>
2010-02-13 15:33:49 -0800

    power button toggles menu

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -36,7 +36,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
         return 1;
-    return key_code == KEY_HOME || key_code == KEY_MENU;
+    return key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END;
 }
 
 int device_reboot_now(volatile char* key_pressed, int key_code) {
