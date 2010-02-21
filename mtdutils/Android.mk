c788c263973e5dd951068a8e5e074c06d72d4366
Koushik K. Dutta <koushd@gmail.com>
2010-02-20 17:25:03 -0800

    more fixes

--- mtdutils/Android.mk
+++ mtdutils/Android.mk
@@ -21,7 +21,14 @@ LOCAL_SHARED_LIBRARIES := libcutils libc
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dump_image.c mtdutils.c mounts.c
+LOCAL_MODULE := dump_image
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := flash_image.c
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLE
 LOCAL_MODULE := recovery_flash_image
 LOCAL_MODULE_TAGS := eng
 LOCAL_STATIC_LIBRARIES := libmtdutils libcutils libc
@@ -33,6 +40,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dump_image.c mtdutils.c mounts.c
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLE
 LOCAL_MODULE := recovery_dump_image
 LOCAL_MODULE_TAGS := eng
 LOCAL_STATIC_LIBRARIES := libcutils libc
@@ -40,12 +48,6 @@ LOCAL_MODULE_STEM := dump_image
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 ADDITIONAL_RECOVERY_EXECUTABLES += recovery_dump_image
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := dump_image.c mtdutils.c mounts.c
-LOCAL_MODULE := dump_image
-LOCAL_MODULE_TAGS := eng
 include $(BUILD_EXECUTABLE)
 
 endif	# TARGET_ARCH == arm
