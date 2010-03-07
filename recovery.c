2bda3e9fa09ccc51e3d5c10134bf91b41a4ab43f
Koushik K. Dutta <koushd@gmail.com>
2010-03-06 16:40:52 -0800

    disable script asserts by default for user usage

--- recovery.c
+++ recovery.c
@@ -547,6 +547,7 @@ main(int argc, char **argv)
         // we are starting up in user initiated recovery here
         // let's set up some default options
         signature_check_enabled = 0;
+        script_assert_enabled = 0;
         is_user_initiated_recovery = 1;
         ui_set_show_text(1);
     }
