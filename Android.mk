6bd595cc32ab939d8044b3bf67323c9b61789f9b
Koushik K. Dutta <koushd@gmail.com>
2010-03-11 22:17:11 -0800

    1.6.4

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ LOCAL_MODULE := recovery
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-RECOVERY_API_VERSION := 1.6.3
+RECOVERY_API_VERSION := 1.6.4
 LOCAL_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
 
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
