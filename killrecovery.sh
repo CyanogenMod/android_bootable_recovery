2098707b0262e999c8ebffdc9e2364c80ad42df5
Koushik Dutta <koushd@gmail.com>
2010-08-16 01:21:10 -0700

    droid x test crap

--- killrecovery.sh
+++ killrecovery.sh
@@ -3,12 +3,17 @@ mkdir -p /sd-ext
 rm /cache/recovery/command
 rm /cache/update.zip
 touch /tmp/.ignorebootmessage
-kill $(ps | grep /sbin/adbd)
-kill $(ps | grep /sbin/recovery)
+#kill $(ps | grep /sbin/adbd)
+#kill $(ps | grep /sbin/recovery)
 
 # On the Galaxy S, the recovery comes test signed, but the
 # recovery is not automatically restarted.
 if [ -f /init.smdkc110.rc ]
+then
+    /sbin/recovery &
+fi
+
+if [ -f init.mapphone_cdma.rc ]
 then
     /sbin/recovery &
 fi
