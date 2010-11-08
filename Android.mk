7bce09f989f099ecb228fb1faedfe489f990cbd7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-11-08 23:54:30 +0000

    recovery: Support board-specified font sizes and font-files
    This, for example, is for an LDPI device:
    BOARD_RECOVERY_FONT_INCLUDE_HEADER := ../../device/geeksphone/one/font_7x16.h
    BOARD_RECOVERY_FONT_SIZE := 7x16
    
    Change-Id: I71d2f420b9668b5767c9b0b5d90e3e523c56e873

--- Android.mk
+++ Android.mk
@@ -58,6 +58,12 @@ $(foreach board_define,$(BOARD_RECOVERY_DEFINES), \
   ) \
   )
 
+ifdef BOARD_RECOVERY_FONT_SIZE
+   BOARD_RECOVERY_FONT_SIZE:=$(subst x, ,$(BOARD_RECOVERY_FONT_SIZE))
+   LOCAL_CFLAGS += -DCHAR_WIDTH=$(word 1,$(BOARD_RECOVERY_FONT_SIZE))
+   LOCAL_CFLAGS += -DCHAR_HEIGHT=$(word 2,$(BOARD_RECOVERY_FONT_SIZE))
+endif
+
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
 # It gets copied there in config/Makefile.  LOCAL_MODULE_TAGS suppresses
 # a (redundant) copy of the binary in /system/bin for user builds.
