7161f35736e6c45f16339462ddff45bcfcba38c0
Koushik Dutta <koushd@gmail.com>
2011-02-27 17:00:47 -0800

    specify a type on the mount.
    Change-Id: I610330e2971aaf235e0420fa7d4387e4f55b0015

--- roots.c
+++ roots.c
@@ -126,7 +126,7 @@ int try_mount(const char* device, const char* mount_point, const char* fs_type, 
     }
     else {
         char mount_cmd[PATH_MAX];
-        sprintf(mount_cmd, "mount -o%s %s %s", fs_options, device, mount_point);
+        sprintf(mount_cmd, "mount -t %s -o%s %s %s", fs_type, fs_options, device, mount_point);
         ret = __system(mount_cmd);
     }
     if (ret == 0)
