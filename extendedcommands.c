b986fe24b45cdcd01bbef787dfea94a911619b24
Koushik Dutta <koushd@gmail.com>
2011-06-27 11:03:04 -0700

    Revert "Reverts: http://review.cyanogenmod.com/#change,6130"
    This reverts commit a42d2b1e2466f69be6d802cd6511c076e0285ad9.

--- extendedcommands.c
+++ extendedcommands.c
@@ -894,10 +894,7 @@ void show_advanced_menu()
         switch (chosen_item)
         {
             case 0:
-#ifdef TARGET_RECOVERY_PRE_COMMAND
-                __system(TARGET_RECOVERY_PRE_COMMAND);
-#endif
-                __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, "recovery");
+                reboot_wrapper("recovery");
                 break;
             case 1:
             {
