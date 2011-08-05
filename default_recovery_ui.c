b525d69498dae8b77b833f9412086b8d39792ac1
Martin Brabham <optedoblivion@gmail.com>
2011-08-04 22:45:37 -0700

    Recovery Key Selection: Add 3d key support for selecting items.
    Change-Id: I4df621487dda01c34c4fc0559fc46e5b197eb968

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -82,6 +82,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CAMERA:
             case KEY_F21:
             case KEY_SEND:
+            case KEY_3D:
                 return SELECT_ITEM;
             
             case KEY_END:
