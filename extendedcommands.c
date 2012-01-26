4c8e2482a6feb63f3449e57205df84b9749fa88c
Koushik Dutta <koushd@gmail.com>
2012-01-26 14:47:34 -0800

    Who approves this stuff... use get_volume_for_path('/sdcard') and check for null...
    Revert "Hide "mount USB storage" option from mount menu when the board has a virtual sdcard"
    
    This reverts commit e510810c71b5d4eb4d779c0db2d131d2b1b556af.

--- extendedcommands.c
+++ extendedcommands.c
@@ -669,7 +669,6 @@ void show_partition_menu()
     static char* confirm_format  = "Confirm format?";
     static char* confirm = "Yes - Format";
     char confirm_string[255];
-    int ptr = 0;
 
     for (;;)
     {
@@ -691,11 +690,8 @@ void show_partition_menu()
 			options[mountable_volumes+i] = e->txt;
 		}
 
-#ifndef BOARD_USES_VIRTUAL_SDCARD
         options[mountable_volumes+formatable_volumes] = "mount USB storage";
-        ptr = 1;
-#endif
-        options[mountable_volumes+formatable_volumes + ptr] = NULL;
+        options[mountable_volumes+formatable_volumes + 1] = NULL;
 
         int chosen_item = get_menu_selection(headers, &options, 0, 0);
         if (chosen_item == GO_BACK)
