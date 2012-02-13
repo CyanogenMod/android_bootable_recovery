6165afdd72875d9d519dec6a84904165e5295526
Robert Burns <burnsra@gmail.com>
2012-02-13 15:28:38 -0500

    Remove random offset message during installation
    Change-Id: I2281a571e37ba9fac2b99806ce9c63e591b4d39f

--- updater/install.c
+++ updater/install.c
@@ -478,7 +478,7 @@ Value* RetouchBinariesFn(const char* name, State* state,
         fclose(f_random);
     }
     random_base = (random_base + random_bits) % 1024;
-    fprintf(ui->cmd_pipe, "ui_print Random offset: 0x%x\n", random_base);
+    // fprintf(ui->cmd_pipe, "ui_print Random offset: 0x%x\n", random_base);
     fprintf(ui->cmd_pipe, "ui_print\n");
 
     // make sure we never randomize to zero; this let's us look at a file
