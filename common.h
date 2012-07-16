4b52b0804ab7058737ea6bc8290d7199c847e1a0
Koushik Dutta <koushd@gmail.com>
2012-07-15 19:41:24 -0700

    Add a background texture. Animate during nandroids. Fix dir count.
    Change-Id: I9d03f72453d20ba8647b6f81821bf71004621011

--- common.h
+++ common.h
@@ -42,6 +42,7 @@ void ui_set_nice(int enabled);
 #define ui_nice_print(...) { ui_set_nice(1); ui_print(__VA_ARGS__); ui_set_nice(0); }
 int ui_was_niced();
 int ui_get_text_cols();
+void ui_increment_frame();
 
 // Display some header text followed by a menu of items, which appears
 // at the top of the screen (in place of any scrolling ui_print()
