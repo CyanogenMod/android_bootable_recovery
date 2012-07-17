39c92797a10216fd23f526b22cd5a9b6607e206c
Koushik Dutta <koushd@gmail.com>
2012-07-17 09:59:43 -0700

    more perm hacks
    Change-Id: I43127d0b15073f73aa98b78482a4a0dfff3c4f2d

--- nandroid.c
+++ nandroid.c
@@ -355,7 +355,8 @@ int nandroid_backup(const char* backup_path)
         return ret;
     }
     
-    sprintf(tmp, "chmod -R 777 %s ; chmod -R u+r,u+w,g+r,g+w,o+r,o+w /sdcard/clockworkmod ; chmod u+x,g+x,o+x /sdcard/clockworkmod/backup ; chmod u+x,g+x,o+x /sdcard/clockworkmod/blobs", backup_path);
+    // TODO: remove this hack in a few weeks.
+    sprintf(tmp, "chmod -R 777 %s ; chmod -R u+r,u+w,g+r,g+w,o+r,o+w /sdcard/clockworkmod ; chmod u+x,g+x,o+x /sdcard/clockworkmod ; chmod u+x,g+x,o+x /sdcard/clockworkmod/backup ; chmod u+x,g+x,o+x /sdcard/clockworkmod/blobs", backup_path);
     __system(tmp);
     sync();
     ui_set_background(BACKGROUND_ICON_NONE);
