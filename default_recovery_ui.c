4c90bc7b52715a19c9de14f7f2bb2d9bd6d7e5c2
Bumble-Bee <bigbeeshane@gmail.com>
2012-10-12 12:06:10 +0100

    recovery : add minadbd and sideload from AOSP
    -- enable "adb sideload <zip>" command

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -24,6 +24,7 @@ char* MENU_HEADERS[] = { NULL };
 
 char* MENU_ITEMS[] = { "reboot system now",
                        "install zip from sdcard",
+                       "install zip from sideload",
                        "wipe data/factory reset",
                        "wipe cache partition",
                        "backup and restore",
