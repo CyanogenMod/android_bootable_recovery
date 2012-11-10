0d56a0e3e10d9505f656a0dc58726a27bd056fbe
Emilio López <turl@tuxfamily.org>
2012-11-10 05:18:19 +0000

    recovery: fix build on x86
    Recovery fails to build for x86 targets without these
    
    Change-Id: Ib40d61a15ef8c3e23c6f667c0d13f744213b34fa

--- mmcutils/Android.mk
+++ mmcutils/Android.mk
@@ -1,5 +1,4 @@
 ifneq ($(TARGET_SIMULATOR),true)
-ifeq ($(TARGET_ARCH),arm)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -12,5 +11,4 @@ LOCAL_MODULE_TAGS := eng
 
 include $(BUILD_STATIC_LIBRARY)
 
-endif	# TARGET_ARCH == arm
 endif	# !TARGET_SIMULATOR
