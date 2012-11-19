5c3219d991f9fa4f1312610f04e5a79c41628c31
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-19 12:56:22 +0000

    recovery: Update for new libext4_utils library name
    Change-Id: I0a820a6f9a9b0e092426e5eaa4760a89fe3ccdf6

--- updater/Android.mk
+++ updater/Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES := $(updater_src_files)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
 LOCAL_CFLAGS += -DUSE_EXT4
 LOCAL_C_INCLUDES += system/extras/ext4_utils
-LOCAL_STATIC_LIBRARIES += libext4_utils libz
+LOCAL_STATIC_LIBRARIES += libext4_utils_static libz
 endif
 
 ifeq ($(HAVE_SELINUX), true)
