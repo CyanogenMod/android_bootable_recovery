a2cac122888ecbf17271fc0437b6c5850de59502
Koushik Dutta <koushd@gmail.com>
2012-11-20 20:08:31 -0800

    Fix menu bug reported by sk8erwitskil: https://github.com/CyanogenMod/android_bootable_recovery/commit/014d02059c8533f7e1b2ff3f1e8fdb5464098c59#commitcomment-2185643
    Change-Id: I790bcc13cd8fe6bf2f0d8c0511f7d50b50873ac8

--- extendedcommands.c
+++ extendedcommands.c
@@ -1034,7 +1034,8 @@ static void choose_backup_format() {
     };
 
     char* list[] = { "dup (default)",
-        "tar"
+        "tar",
+        NULL
     };
 
     int chosen_item = get_menu_selection(headers, list, 0, 0);
