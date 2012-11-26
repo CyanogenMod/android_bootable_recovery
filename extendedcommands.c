714970482ec7e617ef36faec5a68e6c29317cbde
philz-cwm6 <phytowardt@gmail.com>
2012-11-26 03:56:07 +0100

    Fix partition sdcard menu options
    Menus are scrumbled because of the bug in get_filtered_menu_selection when nullifying menus.
    The only cure for now is to never set a menu to null when calling that function.
    Only last menu can be toggled.
    
    Partition options are broken, but will show only for phones with /emmc since all phones
    have /sdcard. Phones with /external_sd are not affected for same reason explained previously.
    
    Taken from PhilZ Touch recovery
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: If5df09be3d8464321cebb4887e84e5c92a965bd1

--- extendedcommands.c
+++ extendedcommands.c
@@ -1324,19 +1324,19 @@ void show_advanced_menu()
                             "show log",
                             "fix permissions",
                             "partition sdcard",
-                            "partition external sdcard",
-                            "partition internal sdcard",
+                            NULL,
                             NULL
     };
 
-    if (!can_partition("/sdcard")) {
-        list[6] = NULL;
-    }
-    if (!can_partition("/external_sd")) {
-        list[7] = NULL;
-    }
-    if (!can_partition("/emmc")) {
-        list[8] = NULL;
+    char *other_sd = NULL;
+    if (volume_for_path("/emmc") != NULL) {
+        other_sd = "/emmc";
+        list[6] = "partition external sdcard";
+        list[7] = "partition internal sdcard";
+    } else if (volume_for_path("/external_sd") != NULL) {
+        other_sd = "/external_sd";
+        list[6] = "partition internal sdcard";
+        list[7] = "partition external sdcard";
     }
 
     for (;;)
@@ -1391,13 +1391,11 @@ void show_advanced_menu()
                 ui_print("Done!\n");
                 break;
             case 6:
-                partition_sdcard("/sdcard");
+                if (can_partition("/sdcard"))
+                    partition_sdcard("/sdcard");
                 break;
             case 7:
-                partition_sdcard("/external_sd");
-                break;
-            case 8:
-                partition_sdcard("/emmc");
+                partition_sdcard(other_sd);
                 break;
         }
     }
