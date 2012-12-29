9455f0c96fde1f05d6c4f07374d29905492441f5
codeworkx <codeworkx@cyanogenmod.org>
2012-12-29 14:49:18 +0100

    minui: board specific events
    Change-Id: Iadfe9a70df249ca52ab6c1e818c1295e7546ac90

--- minui/Android.mk
+++ minui/Android.mk
@@ -1,7 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := events.c resources.c
+LOCAL_SRC_FILES := resources.c
+
+ifneq ($(BOARD_CUSTOM_RECOVERY_EVENTS),)
+  LOCAL_SRC_FILES += $(BOARD_CUSTOM_RECOVERY_EVENTS)
+else
+  LOCAL_SRC_FILES += events.c
+endif
+
 ifneq ($(BOARD_CUSTOM_GRAPHICS),)
   LOCAL_SRC_FILES += $(BOARD_CUSTOM_GRAPHICS)
 else
