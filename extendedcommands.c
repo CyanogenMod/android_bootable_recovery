2162ee6da07785ae8e857eb0d829f3855037e0b8
Matt Mower <mowerm@gmail.com>
2013-05-24 14:25:31 -0500

    Fix compiler warning: redefinition in extendedcommands.c
    ITEM_APPLY_SDCARD is already defined in recovery_ui.h, fortunately
    also as 1.
    
    This suggestion comes from Phil3759.
    
    Change-Id: I9ac370ca9640d61b6b96f60954d35f7aa458d75b

--- extendedcommands.c
+++ extendedcommands.c
@@ -119,7 +119,7 @@ int install_zip(const char* packagefilepath)
 }
 
 #define ITEM_CHOOSE_ZIP       0
-#define ITEM_APPLY_SDCARD     1
+#define ITEM_APPLY_UPDATE     1
 #define ITEM_SIG_CHECK        2
 #define ITEM_CHOOSE_ZIP_INT   3
 
@@ -154,7 +154,7 @@ void show_install_update_menu()
             case ITEM_SIG_CHECK:
                 toggle_signature_check();
                 break;
-            case ITEM_APPLY_SDCARD:
+            case ITEM_APPLY_UPDATE:
             {
                 if (confirm_selection("Confirm install?", "Yes - Install /sdcard/update.zip"))
                     install_zip(SDCARD_UPDATE_FILE);
