593b54c8f46d40f31bc47504fba3a0b9c2cdc573
Koushik Dutta <koushd@gmail.com>
2013-06-18 20:10:19 -0700

    fix stat usage
    Change-Id: Iab3290adca1602a23fc407d2b3f50b9aeac60389

--- nandroid.c
+++ nandroid.c
@@ -597,7 +597,7 @@ int nandroid_restore_partition_extended(const char* backup_path, const char* mou
         restore_handler = tar_extract_wrapper;
         strcpy(tmp, "/proc/self/fd/0");
     }
-    else if (0 != (ret = statfs(tmp, &file_info))) {
+    else if (0 != (ret = stat(tmp, &file_info))) {
         // can't find the backup, it may be the new backup format?
         // iterate through the backup types
         printf("couldn't find default\n");
