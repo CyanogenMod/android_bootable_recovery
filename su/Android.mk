ecd5053b923d2ea3f0dd532255f09b9c0297fcfb
Danny Baumann <dannybaumann@web.de>
2013-09-23 13:22:02 +0200

    Add required libraries to list.
    Change-Id: Ice466de2bddd6050335978d70f31831f4d428576

--- su/Android.mk
+++ su/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := su.recovery
 LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libc
+LOCAL_STATIC_LIBRARIES := libc liblog libcutils
 LOCAL_C_INCLUDES := external/sqlite/dist
 LOCAL_SRC_FILES := ../../../external/koush/Superuser/Superuser/jni/su/su.c ../../../external/koush/Superuser/Superuser/jni/su/daemon.c ../../../external/koush/Superuser/Superuser/jni/su/activity.c ../../../external/koush/Superuser/Superuser/jni/su/utils.c dbstub.c
 LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\"
