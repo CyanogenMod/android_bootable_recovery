262055867469b3d1437396bf9a11573566a39c25
Koushik Dutta <koushd@gmail.com>
2013-10-05 15:24:05 -0700

    Allow sideload to be used in conjunction with other commands.
    Change-Id: I8b35a010e5fdb36cec991c56d5cd0def72865308

--- recovery.c
+++ recovery.c
@@ -1008,14 +1008,6 @@ main(int argc, char **argv) {
     } else if (wipe_cache) {
         if (wipe_cache && erase_volume("/cache")) status = INSTALL_ERROR;
         if (status != INSTALL_SUCCESS) ui_print("Cache wipe failed.\n");
-    } else if (sideload) {
-        signature_check_enabled = 0;
-        if (!headless)
-            ui_set_show_text(1);
-        if (0 == apply_from_adb()) {
-            status = INSTALL_SUCCESS;
-            ui_set_show_text(0);
-        }
     } else {
         LOGI("Checking for extendedcommand...\n");
         status = INSTALL_ERROR;  // No command specified
@@ -1041,6 +1033,16 @@ main(int argc, char **argv) {
             }
         } else {
             LOGI("Skipping execution of extendedcommand, file not found...\n");
+        }
+    }
+
+    if (sideload) {
+        signature_check_enabled = 0;
+        if (!headless)
+            ui_set_show_text(1);
+        if (0 == apply_from_adb()) {
+            status = INSTALL_SUCCESS;
+            ui_set_show_text(0);
         }
     }
 
