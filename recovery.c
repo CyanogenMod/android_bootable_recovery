f505024d68da9fd7f6890d90fe6775388b81f97c
Nathan Grebowiec <njgreb@gmail.com>
2013-10-12 10:04:25 -0500

    Enable swipe based touch controlls when BOARD_HAS_RECOVERY_TOUCH is defined for the device.  Hardware keys always continue to work.
    Patchset 1) Initial set
    Patchset 2) change Board Config variable
                don't add if BOARD_TOUCH_RECOVERY set
    Patchset 3) more ifdef's for all!
    Patchset 4) moved instructions
                even more stuff ifdef'ed
                no more abs name issues
    
    Change-Id: Id9844c38c31bd1985dd78a6d1dd81e71aa1f87f3

--- recovery.c
+++ recovery.c
@@ -910,6 +910,16 @@ main(int argc, char **argv) {
     device_ui_init(&ui_parameters);
     ui_init();
     ui_print(EXPAND(RECOVERY_VERSION)"\n");
+
+#ifdef BOARD_RECOVERY_SWIPE
+#ifndef BOARD_TOUCH_RECOVERY
+    //display directions for swipe controls
+    ui_print("Swipe up/down to change selections.\n");
+    ui_print("Swipe to the right for enter.\n");
+    ui_print("Swipe to the left for back.\n");
+#endif
+#endif
+
     load_volume_table();
     process_volumes();
     vold_client_start(&v_callbacks, 0);
