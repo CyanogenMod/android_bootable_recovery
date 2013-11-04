a16c446aa947c2b94b92c6124f5bba503adcce3e
philz-cwm6 <phytowardt@gmail.com>
2013-11-04 16:06:25 +0100

    clean up reboot to main system and remove old useless variables write recovery version whenever we exit recovery (poweroff can end up into a reboot to main system when device turned back on) Change-Id: Idf8cda0639e3fa5e6073b196a85899c57ca18e9b

--- recovery.c
+++ recovery.c
@@ -75,7 +75,6 @@ static const char *LOG_FILE = "/cache/recovery/log";
 static const char *CACHE_ROOT = "/cache";
 static const char *SDCARD_ROOT = "/sdcard";
 static int allow_display_toggle = 0;
-static int poweroff = 0;
 static const char *TEMPORARY_LOG_FILE = "/tmp/recovery.log";
 static const char *SIDELOAD_TEMP_DIR = "/tmp/sideload";
 
@@ -465,7 +464,7 @@ get_menu_selection(const char** headers, char** items, int menu_only,
     // throw away keys pressed previously, so user doesn't
     // accidentally trigger menu items.
     ui_clear_key_queue();
-    
+
     int item_count = ui_start_menu(headers, items, initial_selection);
     int selected = initial_selection;
     int chosen_item = -1; // NO_ACTION
@@ -711,7 +710,7 @@ prompt_and_wait() {
     for (;;) {
         finish_recovery(NULL);
         ui_reset_progress();
-        
+
         ui_root_menu = 1;
         // ui_menu_level is a legacy variable that i am keeping around to prevent build breakage.
         ui_menu_level = 0;
@@ -732,7 +731,6 @@ prompt_and_wait() {
         for (;;) {
             switch (chosen_item) {
                 case ITEM_REBOOT:
-                    poweroff = 0;
                     return;
 
                 case ITEM_WIPE_DATA:
@@ -817,6 +815,7 @@ setup_adbd() {
 
 // call a clean reboot
 void reboot_main_system(int cmd, int flags, char *arg) {
+    write_recovery_version();
     verify_root_and_recovery();
     finish_recovery(NULL); // sync() in here
     vold_unmount_all();
@@ -954,7 +953,7 @@ main(int argc, char **argv) {
         case 'p': previous_runs = atoi(optarg); break;
         case 's': send_intent = optarg; break;
         case 'u': update_package = optarg; break;
-        case 'w': 
+        case 'w':
 #ifndef BOARD_RECOVERY_ALWAYS_WIPES
         wipe_data = wipe_cache = 1;
 #endif
@@ -1077,25 +1076,15 @@ main(int argc, char **argv) {
         prompt_and_wait();
     }
 
-    verify_root_and_recovery();
-
+    // We reach here when in main menu we choose reboot main system or for some wipe commands on start
     // If there is a radio image pending, reboot now to install it.
     maybe_install_firmware_update(send_intent);
 
     // Otherwise, get ready to boot the main system...
     finish_recovery(send_intent);
+    ui_print("Rebooting...\n");
+    reboot_main_system(ANDROID_RB_RESTART, 0, 0);
 
-    vold_unmount_all();
-
-    sync();
-    if(!poweroff) {
-        ui_print("Rebooting...\n");
-        android_reboot(ANDROID_RB_RESTART, 0, 0);
-    }
-    else {
-        ui_print("Shutting down...\n");
-        android_reboot(ANDROID_RB_POWEROFF, 0, 0);
-    }
     return EXIT_SUCCESS;
 }
 
