efec57a002a97a5c194e37fca607cca5f9c28557
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-12 22:34:39 +0000

    su: Update for latest su daemon compatibility
    Change-Id: I6d2f36d9f01d7bc6004724458875b3dce850d00e

--- su/Android.mk
+++ su/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc liblog libcutils
 LOCAL_C_INCLUDES := external/sqlite/dist
-LOCAL_SRC_FILES := ../../../external/koush/Superuser/Superuser/jni/su/su.c ../../../external/koush/Superuser/Superuser/jni/su/daemon.c ../../../external/koush/Superuser/Superuser/jni/su/activity.c ../../../external/koush/Superuser/Superuser/jni/su/utils.c dbstub.c
+LOCAL_SRC_FILES := ../../../external/koush/Superuser/Superuser/jni/su/su.c ../../../external/koush/Superuser/Superuser/jni/su/daemon.c ../../../external/koush/Superuser/Superuser/jni/su/activity.c ../../../external/koush/Superuser/Superuser/jni/su/utils.c ../../../external/koush/Superuser/Superuser/jni/su/pts.c dbstub.c
 LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\"
 ifdef SUPERUSER_PACKAGE_PREFIX
   LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"$(SUPERUSER_PACKAGE_PREFIX)\"
