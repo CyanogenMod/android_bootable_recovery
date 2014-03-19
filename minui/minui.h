5d473093a970fd0cf236983803467c248542fa94
Steve Kondik <shade@chemlab.org>
2014-03-19 16:37:58 -0700

    recovery: Compatibility with upstream minui change
     * minui is changing a lot upstream. We won't merge it here, but
       it's being used in another branch. Add compatibility in CWM
       so that we can move forward.
    
    Change-Id: I8a3289df2d7bc569229b7212c22adb90d4e76ffd

--- minui/minui.h
+++ minui/minui.h
@@ -73,6 +73,9 @@ void ev_dispatch(void);
 
 // Returns 0 if no error, else negative.
 int res_create_surface(const char* name, gr_surface* pSurface);
+static inline int res_create_display_surface(const char* name, gr_surface* pSurface) {
+    return res_create_surface(name, pSurface);
+}
 void res_free_surface(gr_surface surface);
 
 #endif
