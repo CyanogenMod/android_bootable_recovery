fc90d0f851a02f8929e6078b94ef749817353fa8
xiaolu <server@163.com>
2014-05-02 17:57:18 +0800

    dedupe: backup & restore selinux contexts.
    - patch set 2: remove the comment line, adjust freecon.
    
    Change-Id: Ib691146f5e4d33014aac2b540aefc06657818eb4

--- dedupe/Android.mk
+++ dedupe/Android.mk
@@ -2,29 +2,32 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := dedupe.c driver.c
+LOCAL_SRC_FILES := dedupe.c driver.c \
+    ../../../external/libselinux/src/lsetfilecon.c \
+    ../../../external/libselinux/src/lgetfilecon.c
+
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE := dedupe
-LOCAL_STATIC_LIBRARIES := libcrypto_static
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../external/openssl/include
+LOCAL_STATIC_LIBRARIES := libcrypto_static libselinux
+LOCAL_C_INCLUDES += external/openssl/include external/libselinux/include
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dedupe.c
-LOCAL_STATIC_LIBRARIES := libcrypto_static libcutils libc
+LOCAL_STATIC_LIBRARIES := libcrypto_static libcutils libc libselinux
 LOCAL_MODULE := libdedupe
 LOCAL_MODULE_TAGS := eng
-LOCAL_C_INCLUDES := external/openssl/include
+LOCAL_C_INCLUDES := external/openssl/include external/libselinux/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := driver.c
-LOCAL_STATIC_LIBRARIES := libdedupe libcrypto_static libcutils libc
+LOCAL_STATIC_LIBRARIES := libdedupe libcrypto_static libcutils libc libselinux
 LOCAL_MODULE := utility_dedupe
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_STEM := dedupe
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_C_INCLUDES := external/openssl/include
+LOCAL_C_INCLUDES := external/openssl/include external/libselinux/include
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 LOCAL_FORCE_STATIC_EXECUTABLE := true
