281a228dea06a4290fbdebd95e40286b36673dc0
sssemil <suleymanovemil8@gmail.com>
2014-05-11 13:55:25 +0500

    Add brightness control to recovery
    Change-Id: I11489cf3033675d1343ef0c5b51eadfb50683692

--- extendedcommands.c
+++ extendedcommands.c
@@ -920,6 +920,10 @@ int show_partition_menu() {
                 else
                     ui_print("Done.\n");
                 ignore_data_media_workaround(0);
+
+                // recreate /data/media with proper permissions
+                ensure_path_mounted("/data");
+                setup_data_media();
             }
         } else if (is_data_media() && chosen_item == (mountable_volumes + formatable_volumes + 1)) {
             show_mount_usb_storage_menu();
@@ -1648,7 +1652,7 @@ int show_brightness_menu() {
             }
         }
     }
-    
+
     for (; j > 0; --j) {
         free(list[FIXED_ADVANCED_ENTRIES + j - 1]);
     }
