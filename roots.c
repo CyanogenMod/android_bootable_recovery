7901efe59be2c655b938174d96a39a612225b3ef
philz-cwm6 <phytowardt@gmail.com>
2014-05-28 23:40:34 +0200

    fix nandroid restore to f2fs partitions
    Change-Id: I573342fcdb844a3cb2415e19d498228334c0aca3

--- roots.c
+++ roots.c
@@ -445,10 +445,11 @@ int format_volume(const char* volume) {
 
 #ifdef USE_F2FS
     if (strcmp(v->fs_type, "f2fs") == 0) {
-        const char* args[] = { "mkfs.f2fs", v->blk_device };
-        int result = make_f2fs_main(2, (char**)args);
+        char cmd[PATH_MAX];
+        sprintf(cmd, "mkfs.f2fs %s", v->blk_device);
+        int result = __system(cmd);
         if (result != 0) {
-            LOGE("format_volume: mkfs.f2fs failed on %s\n", v->blk_device);
+            LOGE("format_volume: mkfs.f2fs failed on %s (%s)\n", v->blk_device, strerror(errno));
             return -1;
         }
         return 0;
