5e0251e636389aed1be9df5f7d0d10770930c90b
Matt Mower <mowerm@gmail.com>
2014-06-04 13:03:58 -0500

    bootloader: Silence /misc partition error
    LOGE has been changed to LOGI for "Cannot load volume /misc!"
    since many devices do not define a /misc partition in fstab.
    
    Change-Id: I53c440f820fd4614b0af79e9283baa8c48453373

--- bootloader.cpp
+++ bootloader.cpp
@@ -34,8 +34,8 @@ static int set_bootloader_message_block(const struct bootloader_message *in, con
 int get_bootloader_message(struct bootloader_message *out) {
     Volume* v = volume_for_path("/misc");
     if (v == NULL) {
-      LOGE("Cannot load volume /misc!\n");
-      return -1;
+        LOGI("Cannot load volume /misc.\n");
+        return -1;
     }
     if (strcmp(v->fs_type, "mtd") == 0) {
         return get_bootloader_message_mtd(out, v);
@@ -49,8 +49,8 @@ int get_bootloader_message(struct bootloader_message *out) {
 int set_bootloader_message(const struct bootloader_message *in) {
     Volume* v = volume_for_path("/misc");
     if (v == NULL) {
-      LOGE("Cannot load volume /misc!\n");
-      return -1;
+        LOGI("Cannot load volume /misc.\n");
+        return -1;
     }
     if (strcmp(v->fs_type, "mtd") == 0) {
         return set_bootloader_message_mtd(in, v);
