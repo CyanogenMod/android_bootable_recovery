3367abafd55754fef047affab094d6dd366340a8
Matt Mower <mowerm@gmail.com>
2014-06-29 19:36:18 -0500

    MD5: Abort on errors for cmdline nandroid restore
    If 'nandroid restore' is run from the command line, prompts are not
    possible (thanks to Phyto Wardt for pointing this out). Abort with a
    listing of files that are missing from directory or nandroid.md5.
    
    Change-Id: I527ffeafc5da50e7878ddc103f4fb55218632ce1

--- nandroid_md5.c
+++ nandroid_md5.c
@@ -183,6 +183,7 @@ int nandroid_restore_md5_check(const char *backup_path, unsigned char flags) {
     int ret = 0;
     int filecount = 0;
     int md5count = 0;
+    int use_ui = is_ui_initialized();
 
     if (empty_nandroid_bitmask(flags)) {
         LOGE("Nothing selected for restore.\n");
@@ -286,7 +287,7 @@ int nandroid_restore_md5_check(const char *backup_path, unsigned char flags) {
     }
 
     // Warn user of failure for missing files
-    if (totalmissing) {
+    if (totalmissing && use_ui) {
         int uiback = ui_is_showing_back_button();
         ui_set_showing_back_button(0);
 
@@ -316,6 +317,16 @@ int nandroid_restore_md5_check(const char *backup_path, unsigned char flags) {
             LOGE("Aborting\n");
             goto out;
         }
+    } else if (totalmissing) {
+        // No UI, so no prompt possible; fail the restore
+        ret = -1;
+        LOGE("Backup files are missing:\n");
+        for (i = 0; i < md5count; i++) {
+            if (mm[i].is_missing)
+                LOGE("%s\n", mm[i].filename);
+        }
+        LOGE("Aborting\n");
+        goto out;
     }
 
     // Cross-reference files in directory to those in nandroid.md5
@@ -343,7 +354,7 @@ int nandroid_restore_md5_check(const char *backup_path, unsigned char flags) {
     }
 
     // Warn user about missing md5 references for files
-    if (totalmissing) {
+    if (totalmissing && use_ui) {
         int uiback = ui_is_showing_back_button();
         ui_set_showing_back_button(0);
 
@@ -370,6 +381,16 @@ int nandroid_restore_md5_check(const char *backup_path, unsigned char flags) {
             LOGE("Aborting\n");
             goto out;
         }
+    } else if (totalmissing) {
+        // No UI, so no prompt possible; fail the restore
+        ret = -1;
+        LOGE("Could not find reference MD5 for:\n");
+        for (i = 0; i < filecount; i++) {
+            if (mf[i].is_missing)
+                LOGE("%s\n", mf[i].filename);
+        }
+        LOGE("Aborting\n");
+        goto out;
     }
 
     // Compare MD5s of non-missing files that are selected for restore
