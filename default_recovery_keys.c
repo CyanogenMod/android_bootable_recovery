84968a5565f921124314120517b9b8766d55032a
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-06-30 12:21:19 +0900

    recovery: SEARCH should work as SELECT_ITEM
    Before 2cc22971e40a0ffc7849cdf09dc96916c22136e0, SEARCH worked as
    SELECT_ITEM. After that, it became "back".
    
    It is desirable for SEARCH to work as select because it provides
    a full control for devices with the 4-capacitive-buttons row.
    
    MENU = UP
    HOME = DOWN
    SEARCH = SELECT (was BACK)
    BACK = BACK
    
    Change-Id: Idc70366d76a18edecb25adce2e35e31e814443c5

--- default_recovery_keys.c
+++ default_recovery_keys.c
@@ -27,11 +27,11 @@ int device_handle_key(int key_code, int visible) {
             case KEY_F21:
             case KEY_SEND:
             case KEY_HOMEPAGE:
+            case KEY_SEARCH:
                 return SELECT_ITEM;
             
             case KEY_END:
             case KEY_BACKSPACE:
-            case KEY_SEARCH:
             case KEY_BACK:
                 if (!ui_root_menu) {
                     return GO_BACK;
