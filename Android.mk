7fdfb893239cc0daf2247b856da18a8d9dfc4251
Tom Marshall <tdm@cyngn.com>
2014-12-16 12:04:31 -0800

    recovery: Allow 'mm' and friends to work
    Change-Id: I157fb0a71e859967d7e9b0537b71f2727fbf6f30

--- Android.mk
+++ Android.mk
@@ -148,6 +148,8 @@ BUSYBOX_LINKS := $(shell cat external/busybox/busybox-minimal.links)
 exclude := tune2fs mke2fs
 RECOVERY_BUSYBOX_SYMLINKS := $(addprefix $(TARGET_RECOVERY_ROOT_OUT)/sbin/,$(filter-out $(exclude),$(notdir $(BUSYBOX_LINKS))))
 
+ifeq ($(ONE_SHOT_MAKEFILE),)
+
 LOCAL_ADDITIONAL_DEPENDENCIES += \
     minivold \
     recovery_e2fsck \
@@ -162,6 +164,8 @@ LOCAL_ADDITIONAL_DEPENDENCIES += $(RECOVERY_SYMLINKS) $(RECOVERY_BUSYBOX_SYMLINK
 
 ifneq ($(TARGET_RECOVERY_DEVICE_MODULES),)
     LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_RECOVERY_DEVICE_MODULES)
+endif
+
 endif
 
 include $(BUILD_EXECUTABLE)
