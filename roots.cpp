1f1bf389ee11a1805bfd536455bea6d006550761
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-13 21:25:26 -0600

    recovery: Autodetect filesystem type
    * Multiple fstab lines (supported in android) cause recovery to fail
      to mount partitions if the fs type is not the same as the first
      fstab entry.
    
    * When attempting to blindly mount what should be an ext4/f2fs/vfat
      partition, run a check with blkid to see if we can find a more
      accurate partition type, and if so, use that.
    
    * Also autodetect the filesystem type for mounting from update.zips
      and ignore whatever filesystem is specified there.
    
    Change-Id: Ib6f4535dd88ef714ae1ca6fb0ffae1c7dac0f7ce

--- roots.cpp
+++ roots.cpp
@@ -37,6 +37,7 @@ extern "C" {
 }
 
 #include "voldclient/voldclient.h"
+#include <blkid/blkid.h>
 
 static struct fstab *fstab = NULL;
 
@@ -308,9 +309,15 @@ int ensure_volume_mounted(fstab_rec* v) {
             return -1;
         }
         return mtd_mount_partition(partition, v->mount_point, v->fs_type, 0);
-    } else if (strcmp(v->fs_type, "ext4") == 0 ||
-               strcmp(v->fs_type, "f2fs") == 0 ||
-               strcmp(v->fs_type, "vfat") == 0) {
+    } else {
+        char *detected_fs_type = blkid_get_tag_value(NULL, "TYPE", v->blk_device);
+        if (detected_fs_type) {
+            v->fs_type = detected_fs_type;
+        } else {
+            LOGE("unknown fs_type \"%s\" for %s\n", v->fs_type, v->mount_point);
+            return -1;
+        }
+
         result = mount(v->blk_device, v->mount_point, v->fs_type,
                        MS_NOATIME | MS_NODEV | MS_NODIRATIME, "");
         if (result == 0) return 0;
@@ -318,9 +325,6 @@ int ensure_volume_mounted(fstab_rec* v) {
         LOGE("failed to mount %s (%s)\n", v->mount_point, strerror(errno));
         return -1;
     }
-
-    LOGE("unknown fs_type \"%s\" for %s\n", v->fs_type, v->mount_point);
-    return -1;
 }
 
 int ensure_path_unmounted(const char* path, bool detach) {
