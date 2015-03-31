bdd10d681352144785b1739e871902801be59260
Steve Kondik <steve@cyngn.com>
2015-03-31 05:45:50 -0700

    sr: Clean up navigation
     * Make log view actually work (and not crash)
     * Simplify rendering
     * Use proper background icons
    
    Change-Id: Icbcfcf54f8517a434fc776e5f55243830c3e52e9

--- recovery.cpp
+++ recovery.cpp
@@ -767,6 +767,9 @@ static void file_to_ui(const char* fn) {
             ui->WaitKey();
         }
     }
+
+    ui->SetBackground(RecoveryUI::NO_COMMAND);
+
     redirect_stdio(TEMPORARY_LOG_FILE);
     fclose(fp);
 }
@@ -798,9 +801,11 @@ static void choose_recovery_file(Device* device) {
     entries[0] = strdup("Go back");
     title_headers = prepend_title((const char**)headers);
 
+    ui->SetBackground(RecoveryUI::NO_COMMAND);
+
     while(1) {
         int chosen_item = get_menu_selection(title_headers, entries, 1, 0, device);
-        if (chosen_item == 0) break;
+        if (chosen_item == 0 || chosen_item == Device::kGoBack) break;
         file_to_ui(entries[chosen_item]);
     }
 
@@ -965,14 +970,12 @@ prompt_and_wait(Device* device, int status) {
         finish_recovery(NULL);
         ui_root_menu = 1;
         switch (status) {
-            case INSTALL_SUCCESS:
-            case INSTALL_NONE:
-                ui->SetBackground(RecoveryUI::NONE);
-                break;
-
             case INSTALL_ERROR:
             case INSTALL_CORRUPT:
                 ui->SetBackground(RecoveryUI::ERROR);
+                break;
+            default:
+                ui->SetBackground(RecoveryUI::NO_COMMAND);
                 break;
         }
         ui->SetProgressType(RecoveryUI::EMPTY);
@@ -1261,7 +1264,7 @@ main(int argc, char **argv) {
         ui->SetStage(st_cur, st_max);
     }
 
-    ui->SetBackground(RecoveryUI::NONE);
+    ui->SetBackground(RecoveryUI::NO_COMMAND);
     if (show_text) ui->ShowText(true);
 
     /*enable the backlight*/
