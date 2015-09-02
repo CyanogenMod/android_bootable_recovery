90afeebd4d00b9988a6bc510caafe856e8c9e350
Narayan Kamath <narayan@google.com>
2015-09-02 12:34:52 +0100

    Track usage of Vector / SortedVector from libutils DO NOT MERGE
    bug: 22953624
    Change-Id: Ifcc17e39433ac91ca41da5d336fb3006dfbb65a8

--- Android.mk
+++ Android.mk
@@ -141,7 +141,7 @@ ifeq ($(BOARD_USES_BML_OVER_MTD),true)
 LOCAL_STATIC_LIBRARIES += libbml_over_mtd
 endif
 
-LOCAL_STATIC_LIBRARIES += libminui libpixelflinger_static libpng libcutils liblog
+LOCAL_STATIC_LIBRARIES += libminui libpixelflinger_static libpng libcutils liblog libutils
 LOCAL_STATIC_LIBRARIES += libstdc++ libc
 
 LOCAL_STATIC_LIBRARIES += libselinux
