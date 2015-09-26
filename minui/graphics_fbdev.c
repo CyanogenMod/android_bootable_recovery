5a27addc0907bce4d71b9e87459775f1c72becb3
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-09-26 14:02:46 +0100

    minui: allow to use FBIOPAN_DISPLAY for fb swap
    Change-Id: I9a5d640b1168de518885b89f63499c6f31958b75

--- minui/graphics_fbdev.c
+++ minui/graphics_fbdev.c
@@ -72,7 +72,12 @@ static void set_displayed_framebuffer(unsigned n)
     vi.yres_virtual = gr_framebuffer[0].height * 2;
     vi.yoffset = n * gr_framebuffer[0].height;
     vi.bits_per_pixel = gr_framebuffer[0].pixel_bytes * 8;
+    
+#ifdef BOARD_RECOVERY_NEEDS_FBIOPAN_DISPLAY    
+    if (ioctl(fb_fd, FBIOPAN_DISPLAY, &vi) < 0) {
+#else
     if (ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vi) < 0) {
+#endif
         perror("active fb swap failed");
     }
     displayed_buffer = n;
