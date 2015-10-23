fe5f9c396bd32de8f9eeef596486f0a72dd7fe1a
Pat Erley <perley@cyngn.com>
2015-10-22 18:04:35 -0700

    Blank screen during shutdown and reboot
    Some hardware doesn't like having the panel on and still
    active during pwoer cycles, so just turn it off.
    
    Change-Id: Ied1f0802f5a2d45980ee33abf2456a291ba64beb

--- recovery.cpp
+++ recovery.cpp
@@ -1733,6 +1733,9 @@ int main(int argc, char **argv) {
 
     sync();
 
+    write_file("/sys/class/leds/lcd-backlight/brightness", "0");
+    gr_fb_blank(true);
+
     switch (after) {
         case Device::SHUTDOWN:
             ui->Print("Shutting down...\n");
