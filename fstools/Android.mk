0618bc68155f17ebaaeab9800b7ba414c41549a8
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 23:18:25 +0200

    fstools: Move msdos tools to fstools
    Change-Id: Ida9bf524a85be582ba0a1423c6e43203efc07b0a

--- fstools/Android.mk
+++ fstools/Android.mk
@@ -7,7 +7,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fstools
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-LOCAL_SRC_FILES := fstools.cpp
+LOCAL_SRC_FILES := fstools.cpp \
+    ../../system/core/toolbox/newfs_msdos.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 LOCAL_WHOLE_STATIC_LIBRARIES += \
@@ -35,6 +36,9 @@ LOCAL_WHOLE_STATIC_LIBRARIES += \
 	libf2fs_static \
 	libf2fs_fsck_static \
 	libf2fs_mkfs_static
+
+LOCAL_WHOLE_STATIC_LIBRARIES += \
+	libfsck_msdos_static
 
 LOCAL_STATIC_LIBRARIES := \
 	libext2_blkid \
