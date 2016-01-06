cd60d87be3be8c284e9b8dd9b82da7ef8bcac96c
Pat Erley <perley@cyngn.com>
2016-01-05 16:50:02 -0800

    recovery: Enable the menu for User builds
    Upstream recovery doesn't provide a menu for non ENG/UserDebug
    builds.
    
    OPO-321
    Change-Id: I01b285a40287be4147d15a70b91ad17a3c93da68

--- recovery.cpp
+++ recovery.cpp
@@ -1473,12 +1473,8 @@ main(int argc, char **argv) {
         status = INSTALL_NONE;  // No command specified
         ui->SetBackground(RecoveryUI::NO_COMMAND);
 
-        // http://b/17489952
-        // If this is an eng or userdebug build, automatically turn on the
-        // text display if no command is specified.
-        if (is_ro_debuggable()) {
-            ui->ShowText(true);
-        }
+        // Always show menu if no command is specified
+        ui->ShowText(true);
     }
 
     if (!sideload_auto_reboot && (status == INSTALL_ERROR || status == INSTALL_CORRUPT)) {
