1a426223a6e4890a39514cc275c0e8697705048d
Tom Marshall <tdm@cyngn.com>
2016-02-15 16:41:03 -0800

    recovery: Remove empty header line
    CYNGNOS-2068
    Change-Id: I15d5dc734adba58ca4d44a5818c23115481e0470
    (cherry picked from commit 49879342bc3ba7b7db7721678334c9aaf14dccbf)

--- recovery.cpp
+++ recovery.cpp
@@ -1017,7 +1017,6 @@ refresh:
     }
     if (chosen == item_sideload) {
         static const char* headers[] = {  "ADB Sideload",
-                                    "",
                                     NULL
         };
         static const char* list[] = { "Cancel sideload", NULL };
