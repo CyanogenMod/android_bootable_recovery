5f09cb68963a99a5c0bc4afb56a881da48eb9516
Saleem Rashid <spudowiar@outlook.com>
2016-05-01 07:42:05 +0100

    ninja-build: don't use $(shell) in other Make constructs [4/4]
    Change-Id: I74961c5dab346b78a09e82353e858b9d68ecbf33

--- Android.mk
+++ Android.mk
@@ -188,12 +188,11 @@ include $(BUILD_EXECUTABLE)
 
 # Run toybox-instlist and generate the rest of the symlinks
 toybox_recovery_links: $(TOYBOX_INSTLIST)
-toybox_recovery_links: TOY_LIST=$(shell $(TOYBOX_INSTLIST))
 toybox_recovery_links: TOYBOX_BINARY := $(TARGET_RECOVERY_ROOT_OUT)/sbin/toybox
 toybox_recovery_links:
-	@echo -e ${CL_CYN}"Generate Toybox links:"${CL_RST} $(TOY_LIST)
+	@echo -e ${CL_CYN}"Generate Toybox links:"${CL_RST} $$($(TOYBOX_INSTLIST))
 	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/sbin
-	$(hide) $(foreach t,$(TOY_LIST),ln -sf toybox $(TARGET_RECOVERY_ROOT_OUT)/sbin/$(t);)
+	$(hide) $(TOYBOX_INSTLIST) | xargs -I'{}' ln -sf toybox '$(TARGET_RECOVERY_ROOT_OUT)/sbin/{}'
 
 # mkshrc
 include $(CLEAR_VARS)
